f3e95bb577c755b46b6dbe3e65edcde9aae428ea
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2013-10-18 13:16:42 +0200

    Site updated at 2013-10-18 11:16:42 UTC

--- teaching/ase/files/rob.R
+++ teaching/ase/files/rob.R
@@ -11,8 +11,77 @@ summary_rob <- function(object, alpha = 0.05) {
   sobj$coefficients <- b
   f <- waldtest(object, vcov=vcovHC, test = 'Chisq')
   sobj$fstatistic <- c(value=f$Chisq[2], numdf = abs(f$Df[2]), dendf = Inf)
+  class(sobj) <- "summary_rob"
   sobj
 }
+
+
+print.summary_rob <-
+  function (x, digits = max(3L, getOption("digits") - 3L), symbolic.cor = x$symbolic.cor, 
+    signif.stars = FALSE, ...) 
+{
+    cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"), 
+        "\n\n", sep = "")
+    resid <- x$residuals
+    df <- x$df
+    rdf <- df[2L]
+  
+    if (length(x$aliased) == 0L) {
+        cat("\nNo Coefficients\n")
+    }
+    else {
+        if (nsingular <- df[3L] - df[1L]) 
+            cat("\nCoefficients: (", nsingular, " not defined because of singularities)\n", 
+                sep = "")
+        else cat("\nCoefficients:\n")
+        coefs <- x$coefficients
+        if (!is.null(aliased <- x$aliased) && any(aliased)) {
+            cn <- names(aliased)
+            coefs <- matrix(NA, length(aliased), 4, dimnames = list(cn, 
+                colnames(coefs)))
+            coefs[!aliased, ] <- x$coefficients
+        }
+        printCoefmat(coefs, digits = digits, signif.stars = signif.stars, 
+            na.print = "NA", ...)
+    }
+    cat("\nResidual standard error:", format(signif(x$sigma, 
+        digits)), "on", rdf, "degrees of freedom")
+    cat("\n")
+    if (nzchar(mess <- naprint(x$na.action))) 
+        cat("  (", mess, ")\n", sep = "")
+    if (!is.null(x$fstatistic)) {
+        cat("Multiple R-squared: ", formatC(x$r.squared, digits = digits))
+        cat(",\tAdjusted R-squared: ", formatC(x$adj.r.squared, 
+            digits = digits), "\nF-statistic:", formatC(x$fstatistic[1L], 
+            digits = digits), "on", x$fstatistic[2L], "and", 
+            x$fstatistic[3L], "DF,  p-value:", format.pval(pf(x$fstatistic[1L], 
+                x$fstatistic[2L], x$fstatistic[3L], lower.tail = FALSE), 
+                digits = digits))
+        cat("\n")
+    }
+    correl <- x$correlation
+    if (!is.null(correl)) {
+        p <- NCOL(correl)
+        if (p > 1L) {
+            cat("\nCorrelation of Coefficients:\n")
+            if (is.logical(symbolic.cor) && symbolic.cor) {
+                print(symnum(correl, abbr.colnames = NULL))
+            }
+            else {
+                correl <- format(round(correl, 2), nsmall = 2, 
+                  digits = digits)
+                correl[!lower.tri(correl)] <- ""
+                print(correl[-1, -p, drop = FALSE], quote = FALSE)
+            }
+        }
+    }
+    cat("Robust standard errors \n")
+    cat("\n")
+    invisible(x)
+}
+
+
+
 
 
 
