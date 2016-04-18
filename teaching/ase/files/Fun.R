69358931de5ea50967bfd8d4f01720706bc044f5
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2016-04-18 12:07:56 +0200

    Site updated at 2016-04-18 10:07:56 UTC

--- teaching/ase/files/Fun.R
+++
@@ -1,6 +0,0 @@
-# COPY and PASTE
-ci<-function(x) #this function  provides coefficients, t-statistics, p values and confidence intervalsfor a given regression
-{               # x<-lm(y~x1+x2...)
-  x1<-summary(x)$coef
-  cbind(x1,confint(x))
-}
